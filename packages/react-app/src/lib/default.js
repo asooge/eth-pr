4db0b6f1f33f6f2e2d2d7f54d7bd27f165ed1acf
asooge <34760728+asooge@users.noreply.github.com>
2021-10-26 20:24:54 -0400

    chore(lint): eslint prettier husky (#4)
    * add eslint as dev dependencies
    
    * add typescript and prettier
    
    * install prettier
    
    * format files with prettier
    
    * install husky
    
    * add husky format pre-commit
    
    * add lint-staged
    
    * add lint and format-check to github workflow
    
    * modify eslint indent rule
    
    * fix format and format-check scripts
    
    * disable eslint react/prop-types WalletButton component

--- packages/react-app/src/lib/default.js
+++ packages/react-app/src/lib/default.js
@@ -1,40 +1,47 @@
-import { addresses, abis } from "@project/contracts";
-import { Contract } from "@ethersproject/contracts";
-import { getDefaultProvider } from "@ethersproject/providers";
+import { addresses, abis } from '@project/contracts'
+import { Contract } from '@ethersproject/contracts'
+import { getDefaultProvider } from '@ethersproject/providers'
 
 export const readOnChainData = async (provider) => {
   // Should replace with the end-user wallet, e.g. Metamask
-  const defaultProvider = getDefaultProvider();
+  const defaultProvider = getDefaultProvider()
   // Create an instance of an ethers.js Contract
   // Read more about ethers.js on https://docs.ethers.io/v5/api/contract/contract/
-  const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider);
+  const ceaErc20 = new Contract(addresses.ceaErc20, abis.erc20, defaultProvider)
   // A pre-defined address that owns some CEAERC20 tokens
-  const tokenBalance = await ceaErc20.balanceOf("0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C");
-  console.log({ tokenBalance: tokenBalance.toString() });
+  const tokenBalance = await ceaErc20.balanceOf(
+    '0x3f8CB69d9c0ED01923F11c829BaE4D9a4CB6c82C',
+  )
+  console.log({ tokenBalance: tokenBalance.toString() })
 }
 
-export const fetchAccount = async (provider, account, setAccount, setRendered) => {
+export const fetchAccount = async (
+  provider,
+  account,
+  setAccount,
+  setRendered,
+) => {
   try {
     if (!provider) {
-      return;
+      return
     }
 
     // Load the user's accounts.
-    const accounts = await provider.listAccounts();
-    setAccount(accounts[0]);
+    const accounts = await provider.listAccounts()
+    setAccount(accounts[0])
 
     // Resolve the ENS name for the first account.
-    const name = await provider.lookupAddress(accounts[0]);
+    const name = await provider.lookupAddress(accounts[0])
 
     // Render either the ENS name or the shortened account address.
     if (name) {
-      setRendered(name);
+      setRendered(name)
     } else {
-      setRendered(account.substring(0, 6) + "..." + account.substring(36));
+      setRendered(account.substring(0, 6) + '...' + account.substring(36))
     }
   } catch (err) {
-    setAccount("");
-    setRendered("");
-    console.error(err);
+    setAccount('')
+    setRendered('')
+    console.error(err)
   }
 }
